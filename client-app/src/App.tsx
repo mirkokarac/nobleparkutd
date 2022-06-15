b4e8fad7ff3734ffc873391ec4ccd84394eb1ffa
mirkokarac <mirkokarac@hotmail.com>
2022-06-15 23:36:27 +1000

    Add axios call to get players from api

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const[players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/players').then(response => {
+      console.log(response);
+      setPlayers(response.data);
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {players.map((player: any) => (
+            <li key={player.id}>{player.firstName}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
