6df66ae616571b808b76c8aa8c90f2523ed63cf8
mirkokarac <mirkokarac@hotmail.com>
2022-06-21 22:09:53 +1000

    Add MediatR create players command and handler

---
+++ Application/Players/Create.cs
@@ -0,0 +1,35 @@
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Players
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Player? Player { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                if(request.Player is not null)
+                {
+                    _context.Players.Add(request.Player);
+
+                    await _context.SaveChangesAsync();                    
+                }
+
+                return Unit.Value;
+            }
+        }
+    }
+}
