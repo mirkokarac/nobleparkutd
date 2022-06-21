32b0a2ac08bf6a47296d7785e0f0fd526120a740
mirkokarac <mirkokarac@hotmail.com>
2022-06-21 22:10:19 +1000

    Add CreatePlayer task

--- API/Controllers/PlayersController.cs
+++ API/Controllers/PlayersController.cs
@@ -17,5 +17,11 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query(){Id = id});
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePlayer(Player player)
+        {
+            return Ok(await Mediator.Send(new Create.Command(){Player = player}));
+        }
     }
 }
