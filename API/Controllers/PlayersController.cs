7ad474fad5683a2093f9ce93360adbcd33730285
mirkokarac <mirkokarac@hotmail.com>
2022-06-22 22:28:48 +1000

    Add EditPlayer, DeletePlayer and update references

--- API/Controllers/PlayersController.cs
+++ API/Controllers/PlayersController.cs
@@ -1,4 +1,5 @@
-using Application.Players;
+using Application.Players.Commands;
+using Application.Players.Queries;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,13 +16,27 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Player>> GetPlayer(Guid id)
         {
-            return await Mediator.Send(new Details.Query(){Id = id});
+            return await Mediator.Send(new Details.Query{Id = id});
         }
 
         [HttpPost]
         public async Task<IActionResult> CreatePlayer(Player player)
         {
-            return Ok(await Mediator.Send(new Create.Command(){Player = player}));
+            return Ok(await Mediator.Send(new Create.Command{Player = player}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditPlayer(Guid id, Player player)
+        {
+            player.Id = id;
+
+            return Ok(await Mediator.Send(new Edit.Command{Player = player}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlayer(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
         }
     }
 }
