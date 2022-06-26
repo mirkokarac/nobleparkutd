1455059d4aa4c2ff24eb2af40c8d935497e059b9
mirkokarac <mirkokarac@hotmail.com>
2022-06-26 19:43:09 +1000

    Moved file to app/layout

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+  const[players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/players').then(response => {
+      setPlayers(response.data);
+    })
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Players' />
+        <List>
+          {players.map((player: any) => (
+            <List.Item key={player.id}>{player.firstName}</List.Item>
+          ))}
+        </List>
+    </div>
+  );
+}
+
+export default App;
