2331828acd1ed1af83efa767e1379689ead6aad3
mirkokarac <mirkokarac@hotmail.com>
2022-06-26 19:57:22 +1000

    Update App to use Player interface

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Player } from '../models/player';
 
 function App() {
-  const[players, setPlayers] = useState([]);
+  const[players, setPlayers] = useState<Player[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/players').then(response => {
+    axios.get<Player[]>('http://localhost:5000/api/players').then(response => {
       setPlayers(response.data);
     })
   }, [])
@@ -15,7 +16,7 @@ function App() {
     <div>
       <Header as='h2' icon='users' content='Players' />
         <List>
-          {players.map((player: any) => (
+          {players.map(player => (
             <List.Item key={player.id}>{player.firstName}</List.Item>
           ))}
         </List>
