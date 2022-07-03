080fcc6ac5032c3d823e32a40f7835b38fa209c1
mirko <mirkokarac@hotmail.com>
2022-07-03 21:40:59 +1000

    Update App
    - Add Navbar
    - Add Dashboard
    - Add players, selectedPlayer, selectPlayer, cancelSelectPlayer, editMode,openForm, closeForm, createOrEdit, deletePlayer to dashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Player } from '../models/player';
 import NavBar from './NavBar';
+import PlayerDashboard from '../../features/players/dashboard/PlayerDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   const[players, setPlayers] = useState<Player[]>([]);
+  const[selectedPlayer, setSelectedPlayer] = useState<Player | undefined>(undefined);
+  const[editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Player[]>('http://localhost:5000/api/players').then(response => {
@@ -13,15 +17,57 @@ function App() {
     })
   }, [])
 
+  function handleSelectPlayer(id: string)
+  {
+    setSelectedPlayer(players.find(x => x.id === id));
+  }
+
+  function handleCancelSelectPlayer()
+  {
+    setSelectedPlayer(undefined);
+  }
+
+  function handleFormOpen(id?: string)
+  {
+    id ? handleSelectPlayer(id) : handleCancelSelectPlayer();
+    setEditMode(true);
+  }
+
+  function handleFormClose()
+  {
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditPlayer(player: Player)
+  {
+    player.id 
+      ? setPlayers([...players.filter(x => x.id !== player.id), player])
+      : setPlayers([...players, {...player, id:uuid()}]);
+      setEditMode(false);
+      setSelectedPlayer(player);
+  }
+
+function handleDeletePlayer(id:string){
+  setPlayers([...players.filter(x => x.id !== id)]);
+}
+
   return (
-    <div>
-      <NavBar />
-        <List>
-          {players.map(player => (
-            <List.Item key={player.id}>{player.firstName}</List.Item>
-          ))}
-        </List>
-    </div>
+    <>
+      <NavBar openForm={handleFormOpen} />
+      <Container style={{marginTop: "7em"}}>
+        <PlayerDashboard 
+          players={players}
+          selectedPlayer={selectedPlayer}
+          selectPlayer={handleSelectPlayer}
+          cancelSelectPlayer={handleCancelSelectPlayer}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditPlayer}
+          deletePlayer={handleDeletePlayer}
+        />
+      </Container>
+    </>
   );
 }
 
