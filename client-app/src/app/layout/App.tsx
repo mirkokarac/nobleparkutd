04e4937c89cfd72b0369a487bb2a15a43f6afc92
mirko <mirkokarac@hotmail.com>
2022-07-08 08:35:48 +1000

    Add player details route

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,6 +5,7 @@ import { observer } from 'mobx-react-lite';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../../features/home/Home';
 import PlayerForm from '../../features/players/form/PlayerForm';
+import PlayerDetails from '../../features/players/details/PlayerDetails';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/players' element={<PlayerDashboard />} />
+          <Route path='/players/:id' element={<PlayerDetails />} />
           <Route path='/players/create' element={<PlayerForm />} />
         </Routes>
       </Container>
