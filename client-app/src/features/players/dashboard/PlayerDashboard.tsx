b9c1870e4e77758330812b5757176e66e4a07bfc
mirko <mirkokarac@hotmail.com>
2022-07-07 22:27:21 +1000

    Remove unused code in player dashboard
    - Code not used due to change in workflow due to mobx

--- client-app/src/features/players/dashboard/PlayerDashboard.tsx
+++ client-app/src/features/players/dashboard/PlayerDashboard.tsx
@@ -1,31 +1,18 @@
 import { observer } from "mobx-react-lite";
 import { Grid } from "semantic-ui-react";
-import { Player } from "../../../app/models/player";
 import { useStore } from "../../../app/stores/store";
 import PlayerDetails from "../details/PlayerDetails";
 import PlayerForm from "../form/PlayerForm";
 import PlayerList from "./PlayerList";
 
-interface Props
-{
-    players: Player[];
-    deletePlayer: (id: string) => void;
-    submitting: boolean;
-}
-
-export default observer(function PlayerDashboard({players,
-    deletePlayer, submitting} : Props)
+export default observer(function PlayerDashboard()
 {
     const {playerStore} = useStore();
     const {selectedPlayer, editMode} = playerStore;
     return(
         <Grid>
             <Grid.Column width="10">
-                <PlayerList 
-                    players={players}
-                    deletePlayer={deletePlayer}
-                    submitting={submitting}
-                />
+                <PlayerList />
             </Grid.Column>
             <Grid.Column width="6">
                 {selectedPlayer && !editMode &&
