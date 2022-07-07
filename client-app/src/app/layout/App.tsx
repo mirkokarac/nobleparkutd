f16540997acdb24b7eead3d00485a373c25c56c8
mirko <mirkokarac@hotmail.com>
2022-07-07 22:26:52 +1000

    Remove unused code in App
    - Code not used due to change in workflow due to mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Player } from '../models/player';
 import NavBar from './NavBar';
 import PlayerDashboard from '../../features/players/dashboard/PlayerDashboard';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
@@ -11,21 +9,10 @@ import { observer } from 'mobx-react-lite';
 function App() {
   const{playerStore} = useStore();
 
-  const[players, setPlayers] = useState<Player[]>([]);
-  const[submitting, setSubmitting] = useState(false);
-
   useEffect(() => 
   {
     playerStore.loadPlayers();
   }, [playerStore]);
-
-function handleDeletePlayer(id:string){
-  setSubmitting(true);
-  agent.Players.delete(id).then(() =>{
-    setPlayers([...players.filter(x => x.id !== id)]);
-    setSubmitting(false);
-  });
-}
 
   if(playerStore.loadingInitial) return <LoadingComponent content='Loading app' />
   
@@ -33,11 +20,7 @@ function handleDeletePlayer(id:string){
     <>
       <NavBar />
       <Container style={{marginTop: "7em"}}>
-        <PlayerDashboard 
-          players={playerStore.players}
-          deletePlayer={handleDeletePlayer}
-          submitting={submitting}
-        />
+        <PlayerDashboard />
       </Container>
     </>
   );
