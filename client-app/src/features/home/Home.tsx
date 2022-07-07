9c253f8f00654237fa02cdef2097352b1f291a68
mirko <mirkokarac@hotmail.com>
2022-07-07 23:46:05 +1000

    Add home

---
+++ client-app/src/features/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+export default function Home()
+{
+    return(
+        <Container style={{marginTop: "7em"}}>
+            <h1>Home</h1>
+        </Container>
+    )
+}
