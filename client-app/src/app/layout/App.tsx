a9265e0b9fa499e6f4df5adf89cb135891c7981a
mirko <mirkokarac@hotmail.com>
2022-07-07 23:49:36 +1000

    Remove player dashboard container and associated code/imports

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,16 @@
-import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import PlayerDashboard from '../../features/players/dashboard/PlayerDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../../features/home/Home';
 import PlayerForm from '../../features/players/form/PlayerForm';
 
 function App() {
-  const{playerStore} = useStore();
-
-  useEffect(() => 
-  {
-    playerStore.loadPlayers();
-  }, [playerStore]);
-
-  if(playerStore.loadingInitial) return <LoadingComponent content='Loading app' />
-  
   return (
     <>
       <NavBar />
       <Container style={{marginTop: "7em"}}>
-        <PlayerDashboard />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/players' element={<PlayerDashboard />} />
