3a6f06c8d8e35bb6d2586cebe6a095e11434c164
mirko <mirkokarac@hotmail.com>
2022-07-09 00:43:48 +1000

    Separate Home and Players routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,20 +6,31 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from '../../features/home/Home';
 import PlayerForm from '../../features/players/form/PlayerForm';
 import PlayerDetails from '../../features/players/details/PlayerDetails';
+import { Fragment } from 'react';
 
 function App() {
+  return (
+    <Fragment>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/players/*' element={<Players />} />
+      </Routes>
+    </ Fragment>
+  );
+}
+
+function Players() {
   const location = useLocation();
 
   return (
     <>
       <NavBar />
       <Container style={{marginTop: "7em"}}>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/players' element={<PlayerDashboard />} />
-          <Route path='/players/:id' element={<PlayerDetails />} />
-          <Route path='/players/create' element={<PlayerForm key={location.key} /> } />
-          <Route path='/players/edit/:id' element={<PlayerForm key={location.key} />} />
+        <Routes>          
+          <Route path='/' element={<PlayerDashboard />} />
+          <Route path=':id' element={<PlayerDetails />} />
+          <Route path='create' element={<PlayerForm key={location.key} /> } />
+          <Route path='edit/:id' element={<PlayerForm key={location.key} />} />
         </Routes>
       </Container>
     </>
