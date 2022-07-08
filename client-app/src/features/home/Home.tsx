c0d93e9a0d0d1aab038496d67cf5d622f49e3609
mirko <mirkokarac@hotmail.com>
2022-07-09 00:43:19 +1000

    Refactor Home to add a link to Players

--- client-app/src/features/home/Home.tsx
+++ client-app/src/features/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
 export default function Home()
@@ -6,6 +7,7 @@ export default function Home()
     return(
         <Container style={{marginTop: "7em"}}>
             <h1>Home</h1>
+            <h3>Go to <Link to={`/players`}>players</Link></h3>
         </Container>
     )
 }
