aaa2b7cf521c8279dec2bf1f38affe6c699f6b3b
mirko <mirkokarac@hotmail.com>
2022-07-08 23:52:38 +1000

    Add a key to create and edit route
    There was an issue where the create PlayerForm component was not resetting. Adding a key creates a new instance of the component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,12 +2,14 @@ import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import PlayerDashboard from '../../features/players/dashboard/PlayerDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from '../../features/home/Home';
 import PlayerForm from '../../features/players/form/PlayerForm';
 import PlayerDetails from '../../features/players/details/PlayerDetails';
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <NavBar />
@@ -16,8 +18,8 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/players' element={<PlayerDashboard />} />
           <Route path='/players/:id' element={<PlayerDetails />} />
-          <Route path='/players/create' element={<PlayerForm />} />
-          <Route path='/players/edit/:id' element={<PlayerForm />} />
+          <Route path='/players/create' element={<PlayerForm key={location.key} /> } />
+          <Route path='/players/edit/:id' element={<PlayerForm key={location.key} />} />
         </Routes>
       </Container>
     </>
