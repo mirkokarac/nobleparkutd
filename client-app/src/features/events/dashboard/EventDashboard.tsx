44ba355dc0b6d04b135e9c7da10ed3773dca98b8
mirko <mirkokarac@hotmail.com>
2022-07-12 23:29:20 +1000

    Add event dashboard

---
+++ client-app/src/features/events/dashboard/EventDashboard.tsx
@@ -0,0 +1,30 @@
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import { Grid } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useStore } from "../../../app/stores/store";
+import EventList from "./EventList";
+
+export default observer(function EventDashboard()
+{
+    const {eventStore} = useStore();
+    const {loadEvents, eventRegistry} = eventStore;
+
+    useEffect(() => 
+    {
+      if (eventRegistry.size <= 1) loadEvents();
+    }, [eventRegistry.size, loadEvents]);
+  
+    if(eventStore.loadingInitial) return <LoadingComponent content='Loading app' />
+
+    return(
+        <Grid>
+            <Grid.Column width="10">
+                <EventList />
+            </Grid.Column>
+            <Grid.Column width="6">
+                <h2>Event filter</h2>
+            </Grid.Column>
+        </Grid>
+    )
+});
