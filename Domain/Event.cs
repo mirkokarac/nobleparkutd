ebdac869bf250349bbf8afe65395af6ea67d85fa
mirko <mirkokarac@hotmail.com>
2022-07-13 22:48:54 +1000

    Add LocationId, TeamId and AddressId

--- Domain/Event.cs
+++ Domain/Event.cs
@@ -8,9 +8,11 @@ namespace Domain
         public DateTime? EventDate { get; set; }
         public DateTime? EventTime { get; set; }
         public EventType EventType { get; set; } = EventType.Match;
+        public Guid LocationId { get; set; }
         public Location? Location { get; set; }
         public string ImageUrl { get; set; } = String.Empty;
         public bool Completed { get; set; } = false;
+        public Guid TeamId { get; set; }
         public Team? Team { get; set; }
     }
 
@@ -24,6 +26,7 @@ namespace Domain
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = String.Empty;
+        public Guid AddressId { get; set; }
         public Address? Address { get; set; }
         public float Long { get; set; }
         public float Lat { get; set; }
