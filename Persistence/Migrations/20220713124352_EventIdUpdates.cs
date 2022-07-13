f7cbf24a54ad960b283c0630681c6670205c2394
mirko <mirkokarac@hotmail.com>
2022-07-13 22:49:40 +1000

    Add Event Id migration
    LocationId, TeamId and Address Id

---
+++ Persistence/Migrations/20220713124352_EventIdUpdates.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class EventIdUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Location_LocationId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Team_TeamId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Location_Address_AddressId",
+                table: "Location");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AddressId",
+                table: "Location",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TeamId",
+                table: "Events",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LocationId",
+                table: "Events",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Location_LocationId",
+                table: "Events",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Team_TeamId",
+                table: "Events",
+                column: "TeamId",
+                principalTable: "Team",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Location_Address_AddressId",
+                table: "Location",
+                column: "AddressId",
+                principalTable: "Address",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Location_LocationId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Team_TeamId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Location_Address_AddressId",
+                table: "Location");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AddressId",
+                table: "Location",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TeamId",
+                table: "Events",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LocationId",
+                table: "Events",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Location_LocationId",
+                table: "Events",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Team_TeamId",
+                table: "Events",
+                column: "TeamId",
+                principalTable: "Team",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Location_Address_AddressId",
+                table: "Location",
+                column: "AddressId",
+                principalTable: "Address",
+                principalColumn: "Id");
+        }
+    }
+}
