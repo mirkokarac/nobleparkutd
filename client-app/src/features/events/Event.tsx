96785e935080a71232c11480149fca5293927a5a
Mirko Karac <mirkokarac@hotmail.com>
2022-09-23 16:21:47 +1000

    Add Event
    Used to be event list item

---
+++ client-app/src/features/events/Event.tsx
@@ -0,0 +1,47 @@
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Button, Card, Image } from "semantic-ui-react";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+import { useStore } from "../../app/stores/store";
+
+export default observer(function Event() 
+{
+    const { eventStore } = useStore();
+    const { selectedEvent: event, loadEvent, loadingInitial } = eventStore;
+    const {id} = useParams<{id: string}>();
+
+    useEffect(() => {
+        console.log(id)
+        if (id) loadEvent(id);
+    }, [id, loadEvent]);
+
+    if (loadingInitial || !event) return <LoadingComponent />;
+        
+    return (
+        <Card fluid>
+        <Card.Content>
+            { <Image src={`/assets/placeholder.png`} style={{marginBottom: "1em"}}></Image> }
+            <Card.Header>{event.title}</Card.Header>
+            <Card.Meta>
+                <span>{event.id}</span>
+            </Card.Meta>
+            <Card.Description>
+                { event.description}
+            </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+            <Button.Group widths={2}>
+                <Button
+                    as={Link} to={`/events/edit/${event.id}`}          
+                    basic color="blue" content='Edit' 
+                />
+                <Button
+                    as={Link} to={`/events`}
+                    basic color="grey" content='Cancel' 
+                />
+            </Button.Group>
+        </Card.Content>
+  </Card>
+    )
+});
