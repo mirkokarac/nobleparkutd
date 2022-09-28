a5d55e33b6276667074f2933a70454f2389d73a1
mirko <mirkokarac@hotmail.com>
2022-09-28 23:23:32 +1000

    Update event details references
    remove unused and add new ones

--- client-app/src/features/events/details/EventDetails.tsx
+++ client-app/src/features/events/details/EventDetails.tsx
@@ -1,9 +1,13 @@
 import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import { Button, Card, Image } from "semantic-ui-react";
+import { useParams } from "react-router-dom";
+import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
+import EventDetailedChat from "./EventDetailedChat";
+import EventDetailedHeader from "./EventDetailedHeader";
+import EventDetailedInfo from "./EventDetailedInfo";
+import EventDetailedSidebar from "./EventDetailedSidebar";
 
 export default observer(function Event() 
 {
