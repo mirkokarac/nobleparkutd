7b9dd0cb4d137cc56d3af1f78a6e61eaf36bf220
mirko <mirkokarac@hotmail.com>
2022-09-28 22:32:46 +1000

    Add EventItem to Events

--- client-app/src/features/events/Events.tsx
+++ client-app/src/features/events/Events.tsx
@@ -1,50 +1,30 @@
 import { observer } from "mobx-react-lite";
-import { useState, SyntheticEvent } from "react";
-import { Link } from "react-router-dom";
-import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { Fragment } from "react";
+import { Header, Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
+import EventItem from "./EventItem";
 
 export default observer(function Events()
 {   
     const {eventStore} = useStore();
-    const {eventsByTitle, deleteEvent, loading } = eventStore;
-    const[target, setTarget] = useState("");
-
-    function handleEventDelete(e: SyntheticEvent<HTMLButtonElement>, id: string)
-    {
-        setTarget(e.currentTarget.name);
-        deleteEvent(id);
-    }    
+    const {groupedEvents} = eventStore;
+  
     return(
-        <Segment>
-            <Item.Group divided>
-                {eventsByTitle.map(event =>(
-                    <Item key={event.id}>
-                        <Item.Content>
-                            <Item.Header as="a">
-                                {event.title}
-                            </Item.Header>
-                            <Item.Meta>{event.id}</Item.Meta>
-                            <Item.Description>
-                                {event.description}
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button as={Link} to={`/events/${event.id}`}
-                                    floated="right" content="View" 
-                                    className="nbu-blue-bg nbu-white" />                                
-                                <Button 
-                                    name={event.id}
-                                    onClick={(e) => handleEventDelete(e, event.id)} 
-                                    floated="right" content="Delete" 
-                                    className="nbu-red-bg nbu-white"
-                                    loading={loading && target === event.id} 
-                                />
-                                <Label basic content={event.title} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-            </Item.Group>
-        </Segment>
+        <>
+        {groupedEvents.map(([group, events]) => (
+            <Fragment key={group}>
+                <Header sub color="teal">
+                    {group}
+                </Header>
+                <Segment>
+                    <Item.Group divided>
+                        {events.map(event => (
+                            <EventItem key={event.id} event={event} />
+                        ))}
+                    </Item.Group>
+                </Segment>                
+            </Fragment>
+        ))}
+        </>
     )
 });
